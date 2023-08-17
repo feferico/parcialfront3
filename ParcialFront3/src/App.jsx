a4334b3736de3026e95e65c3fc5c87a0e0d19c45
feferico <federico.andres.saavedra@gmail.com>
2023-08-16 21:07:36 -0300

    parcial front 3

---
+++ ParcialFront3/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from './components/Card.jsx';
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App
